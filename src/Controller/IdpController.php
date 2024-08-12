4298a694addfd877911bffcd522c87c1612c8962
tomboone <tbc@tomboone.com>
2024-08-12 10:28:03 +0100

    move flatfile and create tables pages to idpcontroller

--- src/Controller/IdpController.php
+++ src/Controller/IdpController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\AladinError;
+use SimpleSAML\Configuration;
 use SimpleSAML\Error\Exception;
 use SimpleSAML\Metadata\MetaDataStorageHandler;
 use SimpleSAML\Metadata\MetaDataStorageHandlerPdo;
@@ -73,6 +74,109 @@ class IdpController extends AbstractController
         return $this->render('idps/delete_idp.html.twig', [
             'form' => $form->createView(),
             'entityid' => $entityid,
+        ]);
+    }
+
+    /**
+     * Generate PDO tables.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @throws Exception
+     * @throws \Exception
+     */
+    #[Route('/config/pdo', name: 'generate_pdo_tables')]
+    public function generatePdoTables(Request $request): Response
+    {
+        $auth = new Auth();  // Create a new Auth object
+        $auth->requireAdmin();  // Require authentication
+
+        $form = $this->createFormBuilder()
+            ->add('generate', SubmitType::class, ['label' => 'Create SSP PDO Tables', 'attr' => ['class' => 'btn btn-primary']])
+            ->getForm();  // Create the config form
+        $form->handleRequest($request);  // Handle the request
+
+        if ($form->isSubmitted() && $form->isValid()) {  // If the form is submitted and valid
+            $sspConfig = Configuration::getConfig();  // Get the SimpleSAML configuration
+            $sources = $sspConfig->getConfigItem('metadata.sources')->toArray();  // Dump the SimpleSAML configuration
+            # Iterate through configured metadata sources and ensure
+            # that a PDO source exists.
+            foreach ($sources as $source) {
+                # If pdo is configured, create the new handler and initialize the DB.
+                if ($source['type'] === "pdo") {
+                    $metadataStorageHandler = new MetaDataStorageHandlerPdo($source);
+                    $result = $metadataStorageHandler->initDatabase();
+
+                    if ($result === false) {
+                        $this->addFlash('error', 'Failed to initialize metadata database.');  // Add an error flash message
+                    } else {
+                        $this->addFlash('success', 'Successfully initialized metadata database.');  // Add a success flash message
+                    }
+                }
+            }
+            return $this->redirectToRoute('config');  // Redirect to the config page
+        }
+
+        return $this->render('idps/generate_pdo.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * Convert flatfile to PDO.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @throws Exception
+     * @throws \Exception
+     */
+    #[Route('/config/flatfile', name: 'convert_flatfile')]
+    public function convertFlatfile(Request $request): Response
+    {
+        $auth = new Auth();  // Create a new Auth object
+        $auth->requireAdmin();  // Require authentication
+
+        $form = $this->createFormBuilder()
+            ->add('convert', SubmitType::class, [
+                'label' => 'Convert Flatfile to PDO',
+                'attr' => ['class' => 'btn btn-primary']
+            ])
+            ->getForm();  // Create the config form
+        $form->handleRequest($request);  // Handle the request
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sspConfig = Configuration::getConfig();  // Get the SimpleSAML configuration
+            $metadataDir = $sspConfig->getValue('metadatadir');  // Get the SimpleSAML configuration directory
+            $sources = $sspConfig->getConfigItem('metadata.sources')->toArray();  // Dump the SimpleSAML configuration
+            # Iterate through configured metadata sources and ensure
+            # that a PDO source exists.
+            foreach ($sources as $source) {
+                # If pdo is configured, create the new handler and initialize the DB.
+                if ($source['type'] === "pdo") {
+                    $metadataStorageHandler = new MetaDataStorageHandlerPdo($source);
+                    $metadataStorageHandler->initDatabase();
+
+                    $filename = $metadataDir . '/saml20-idp-remote.php';  // Get the metadata file
+                    $metadata = [];
+                    require_once $filename;  // Require the saml20-idp-remote.php file
+                    $set = basename($filename, ".php");  // Get the set name
+                    foreach ($metadata as $key => $value) {
+                        $metadataStorageHandler->addEntry($key, $set, $value);  // Add the metadata entry
+                    }
+                }
+            }
+
+            $this->addFlash('success', 'Successfully converted flatfile to PDO.');  // Add a success flash message
+
+            return $this->redirectToRoute('config');  // Redirect to the config page
+        }
+
+        return $this->render('idps/flatfile.html.twig', [
+            'form' => $form,
         ]);
     }
 
