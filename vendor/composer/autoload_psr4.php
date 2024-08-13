f812ad1fe615e5c37d1d23b0938f1319136f2b8f
Tom Boone <boone@wrlc.org>
2024-08-13 14:57:35 +0000

    switch from fontawesome to svg

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -13,6 +13,7 @@ return array(
     'Twig\\' => array($vendorDir . '/twig/twig/src'),
     'Symfony\\UX\\Turbo\\' => array($vendorDir . '/symfony/ux-turbo/src'),
     'Symfony\\UX\\StimulusBundle\\' => array($vendorDir . '/symfony/stimulus-bundle/src'),
+    'Symfony\\UX\\Icons\\' => array($vendorDir . '/symfony/ux-icons/src'),
     'Symfony\\Runtime\\Symfony\\Component\\' => array($vendorDir . '/symfony/runtime/Internal'),
     'Symfony\\Polyfill\\Php83\\' => array($vendorDir . '/symfony/polyfill-php83'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
