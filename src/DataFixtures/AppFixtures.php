c512d22d5edcd870886aed5318ce9e44393add3a
Tom Boone <boone@wrlc.org>
2024-09-18 11:45:17 +0000

    remove config fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Config;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,40 +9,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $config_names = [
-            [
-                'name' => 'patron_authorization_url',
-                'label' => 'Patron Authorization URL',
-            ],
-            [
-                'name' => 'memcached_host',
-                'label' => 'Memcached Host',
-            ],
-            [
-                'name' => 'memcached_port',
-                'label' => 'Memcached Port',
-            ],
-            [
-                'name' => 'service_provider_name',
-                'label' => 'SSP Service Provider',
-            ],
-            [
-                'name' => 'cookie_prefix',
-                'label' => 'Cookie Prefix',
-            ],
-            [
-                'name' => 'cookie_domain',
-                'label' => 'Cookie Domain',
-            ],
-        ];
-
-        foreach ($config_names as $config_name) {
-            $config = new Config();
-            $config->setName($config_name['name']);
-            $config->setLabel($config_name['label']);
-            $manager->persist($config);
-        }
 
         $manager->flush();
+
     }
 }
