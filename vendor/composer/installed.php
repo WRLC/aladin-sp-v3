ee8f3416bbeff4523a30d71adaab72fee4a08e88
tomboone <tbc@tomboone.com>
2024-10-16 14:41:16 +0000

    install php_codesniffer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '931ae2e90e49bd20edd30c5f0eeb4312ffde2578',
+        'reference' => '71afe18ffc43ee79e4ea4dbcdf0869fd0d7eacbd',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '931ae2e90e49bd20edd30c5f0eeb4312ffde2578',
+            'reference' => '71afe18ffc43ee79e4ea4dbcdf0869fd0d7eacbd',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -817,6 +817,15 @@
             'install_path' => __DIR__ . '/../simplesamlphp/xml-security',
             'aliases' => array(),
             'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.10.3',
+            'version' => '3.10.3.0',
+            'reference' => '62d32998e820bddc40f99f8251958aed187a5c9c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
         ),
         'symfony/apache-pack' => array(
             'pretty_version' => 'v1.0.1',
