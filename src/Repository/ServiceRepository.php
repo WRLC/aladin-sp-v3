9f0c1d9c8c643d98636850fa444fae4293f1d755
tomboone <tbc@tomboone.com>
2024-10-31 13:38:34 +0000

    phpstan fixes for Repository classes

--- src/Repository/ServiceRepository.php
+++ src/Repository/ServiceRepository.php
@@ -7,37 +7,19 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
+ * Class ServiceRepository
+ *
  * @extends ServiceEntityRepository<Service>
  */
 class ServiceRepository extends ServiceEntityRepository
 {
+    /**
+     * ServiceRepository constructor
+     *
+     * @param ManagerRegistry $registry
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Service::class);
     }
-
-    //    /**
-    //     * @return Service[] Returns an array of Service objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Service
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
