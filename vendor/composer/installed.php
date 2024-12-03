f50c1d467112dd62387c428ce3b0515be9327f0c
tomboone <tbc@tomboone.com>
2024-12-03 10:59:48 +0100

    update doctrine/dbal

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '34c24059f50f5f1fa5054dc35237728302a03fb2',
+        'reference' => '11335cdd9501e01ee2bcc7d4e2bc709827d63cea',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '34c24059f50f5f1fa5054dc35237728302a03fb2',
+            'reference' => '11335cdd9501e01ee2bcc7d4e2bc709827d63cea',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -583,8 +583,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache-implementation' => array(
